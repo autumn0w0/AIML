29f1b92b988d23a80ffa72a6cdd4b964022de513
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-03-14 16:17:49 +0530

    push

---
+++ variables and data types/operators.py
@@ -0,0 +1,9 @@
+#arithmetic operators
+a = 10
+b = 5
+print(a + b) # addition
+print(a - b) # subtraction  
+print(a * b) # multiplication
+print(a / b) # division
+print(a % b) # modulus
+print(a ** b) # exponentiation
---
+++ variables and data types/ternary_op.py
@@ -0,0 +1,7 @@
+# food = input("enter your favourite food: ")
+# eat = "eat" if food == "pizza" else "don't eat"
+# print("I", eat, food)
+
+age = int(input("enter your age: "))
+vote = ("yes", "no")[age < 18]
+print("can you vote?", vote)
