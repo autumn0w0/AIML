2d371a9768a5516c5d8c2d334ada8d39e7d3acff
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-03-16 15:56:16 +0530

    update

--- variables and data types/operators.py
+++ variables and data types/operators.py
@@ -1,9 +1,19 @@
-#arithmetic operators
 a = 10
 b = 5
-print(a + b) # addition
-print(a - b) # subtraction  
-print(a * b) # multiplication
-print(a / b) # division
-print(a % b) # modulus
-print(a ** b) # exponentiation
+
+#arithmetic operators
+# print(a + b) # addition
+# print(a - b) # subtraction  
+# print(a * b) # multiplication
+# print(a / b) # division
+# print(a % b) # modulus
+# print(a ** b) # exponentiation
+
+#relational operators
+print(a == b) # equal to
+print(a != b) # not equal to
+print(a > b) # greater than
+print(a < b) # less than    
+print(a >= b) # greater than or equal to
+print(a <= b) # less than or equal to   
+
