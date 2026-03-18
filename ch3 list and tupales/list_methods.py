605f350595a9a2dcc0b938820277d2fb00b84330
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-03-18 21:27:26 +0530

    renaming

---
+++ ch3 list and tupales/list_methods.py
@@ -0,0 +1,11 @@
+l = [1, 2, 3, 4, 5]
+l.append(6)  # Adds 6 to the end of the list
+print(l)
+l.insert(2, 10)  # Inserts 10 at index 2
+print(l)
+l.sort()  # Sorts the list in ascending order
+print(l)
+l.sort(reverse=True)  # Sorts the list in descending order
+print(l)
+l.reverse()  # Reverses the order of the list
+print(l)
