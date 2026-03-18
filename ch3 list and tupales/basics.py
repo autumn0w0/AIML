82394fd3ce3210c7689a51ee13b9f7aec944331b
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-03-18 21:12:10 +0530

    fix

---
+++ ch3 list and tupales/basics.py
@@ -0,0 +1,9 @@
+marks = [1, 2, 3, 4, 5]
+print(marks)
+print(type(marks))
+
+print(marks[0])
+print(len(marks))
+
+marks[0] = 10
+print(marks)
---
+++ ch3 list and tupales/slicing.py
@@ -0,0 +1,6 @@
+marks = [90, 85, 78, 92, 88]
+print(marks[1:4])  # Slicing from index 1 to 3
+print(marks[:3])   # Slicing from the beginning to index 2
+print(marks[2:])   # Slicing from index 2 to the end
+print(marks[-3:-1])  # Slicing the last three elements
+print(marks[2:len(marks)])  # Slicing from index 2 to the end using len() function
