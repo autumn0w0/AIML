d9f11abae81f87624ee2d6c42412df712ea81939
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-03-30 17:43:36 +0530

    set

---
+++ ch4 dictionary and set/set_basics.py
@@ -0,0 +1,8 @@
+nums = {1, 2, 3, 4, 5}
+print(nums)
+
+set2 = {1, 2, 2, 3}
+print(set2)
+
+null_set = set()
+print(null_set)
---
+++ ch4 dictionary and set/set_methods.py
@@ -0,0 +1,10 @@
+set = {1, 2, 3, 4, 5}
+
+print(set.add(6))
+print(set.remove(3))
+print(set.pop())
+set.clear()
+
+set2 = {1, 2, 3, 9, 10}
+print(set2.union(set))
+print(set2.intersection(set))
