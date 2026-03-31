5ab620024bfc86bb7bf3a14e6b1294d3b9872a7d
Akhilesh damke <114846299+autumn0w0@users.noreply.github.com>
2026-03-31 15:37:24 +0530

    fix

--- ch5 loops/loops_basic.py
+++ ch5 loops/loops_basic.py
@@ -1,9 +1,18 @@
-count = 1
-while count <= 5:
-    print(count)
-    count += 1  
+# count = 1
+# while count <= 5:
+#     print(count)
+#     count += 1  
 
-i = 5
-while i >= 1:
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+i = 1
+while i <= 10:
     print(i)
-    i -= 1
+    if i == 4:
+        break
+    i += 1
+print("Loop ended")
+    
